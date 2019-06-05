80df59b6ccc7bf358ad7f60e920f132836acd4b4
prismistim <prismistim@hotmail.com>
2019-06-05 14:11:16 +0900

    update folder name

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = [
     entry: './src/app.js',
     output: {
       filename: 'bundle.js',
-      path: path.join(__dirname, './dist')
+      path: path.join(__dirname, './docs')
     },
     resolve: {
       alias: {
